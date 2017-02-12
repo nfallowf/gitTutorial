ad10304c01f6ed07c9471adaa43058e54d166aaa
nfall <nfallowf@uwo.ca>
2017-02-12 16:58:55 -0500

    Add course class, changes to instructor and student classes

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by nfall on 2017-02-12.
+ */
+public class Course {
+    String cName;
+    String description;
+
+    public Course(String cName, String description) {
+        this.cName = cName;
+        this.description = description;
+    }
+}
